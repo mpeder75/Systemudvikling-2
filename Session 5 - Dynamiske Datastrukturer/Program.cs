55330822054e5cf8f1d8d62c0ac3e5f6f0915003
Mpeder <mpeder75@gmail.com>
2024-02-21 13:01:29 +0100

    Session 5

---
+++ Session 5 - Dynamiske Datastrukturer/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+// Her oprettes instans af LinkedList
+LinkedList s = new LinkedList();  // 1
+
+// Her tilføjes elementer til LinkedList
+s.Add(3);   // 3.
+s.Add(7);
+s.Add(5);
+s.Add(6);
+s.PrintAll();
+
+
+class LinkedList
+{
+    private Node head;
+
+    public LinkedList()
+    {
+        head = null;  // 2. 
+    }
+
+    public void Add(int data)  // 4.
+    {
+        Node toAdd = new Node(data,head);
+        head = toAdd;
+    }
+
+    public void PrintAll()  // 6.
+    {
+        Node current = head;
+        while (current != null)
+        {
+            Console.WriteLine("tmp.data =" + current.Data);
+            current = current.Next;
+        }
+    }
+}
+
+
+public class Node // 5
+{
+    public int Data { get; }
+    public Node Next { get; }
+
+    public Node(int data, Node next)
+    {
+        this.Data = data;
+        this.Next = next;
+    }
+}
