dfe00048e9b628a00f87b7765e48cf00059edb6d
Mpeder <mpeder75@gmail.com>
2024-04-17 10:16:04 +0200

    local changes

---
+++ Session 17 - Operativsystemer/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Session_17___Operativsystemer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            System.Diagnostics.Process.Start("xcopy", "\"c:\\test1\\*.*\" c:\\"+"\"\\test2\\*.*\""+" /S/Y");
+        }
+    }
+}
